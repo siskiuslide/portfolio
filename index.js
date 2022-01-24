9d00a5591f5998f4384a93c3de1a4057a5fd73a4
siskiuslide <jakewhatling2002@gmail.com>
2022-01-24 15:45:55 +0000

    Added more updates and styling

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const checkExtended = (updates) =>{
     return returnVal
 }
 let extended = checkExtended(Array.from(document.querySelectorAll('.update')))
-console.log('before click',extended)
 moreUpdates.addEventListener('click',(e)=>{
     //show the hidden updates
     updates.forEach((update, i) =>{
-        console.log(i)
         if(extended === false){
             if(update.classList.contains('d-none')){
                 update.classList.remove('d-none')
@@ -19,7 +17,7 @@ moreUpdates.addEventListener('click',(e)=>{
             }
         }
         else{
-            if(i > 2 ){
+            if(i > 3 ){
                 update.classList.add('d-none')
                 update.classList.remove('d-flex')
             }
@@ -27,6 +25,5 @@ moreUpdates.addEventListener('click',(e)=>{
     })
 
    extended = checkExtended(Array.from(document.querySelectorAll('.update')))
-   console.log('after click extended', extended)
    extended === true ? e.target.textContent = 'Less' : e.target.textContent = 'More'
 })
